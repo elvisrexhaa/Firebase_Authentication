7398f322dc7aa8846215ff23e05ae173ddb46785
elvisrexhaa <elvis2000@hotmail.co.uk>
2024-03-15 14:21:30 +0000

    Initial Commit

---
+++ Firebase_Authentication/Firebase_AuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_AuthenticationApp.swift
+//  Firebase_Authentication
+//
+//  Created by Elvis Rexha on 15/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_AuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
